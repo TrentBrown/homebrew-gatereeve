0510db8f730dd67874babe12d002b55e35b91ac7
Trent Brown <trent@aintlifegrand.com>
2026-09-01 16:08:22 +0000

    Publish GateReeve 0.1.0-rc.6 Homebrew Cask

--- Casks/gatereeve.rb
+++ Casks/gatereeve.rb
@@ -1,8 +1,8 @@
 cask "gatereeve" do
-  version "0.1.0-rc.2"
-  sha256 "ec50610dfbeffe9bf0004f313e1413ae6d62c58a88cc3b0fa2c25b30b280754f"
+  version "0.1.0-rc.6"
+  sha256 "47121af4f246dbef0d6597c9361df346baacf128d3042fa122a2c8d83772e314"
 
-  url "https://github.com/TrentBrown/gatereeve/releases/download/v0.1.0-rc.2/GateReeve-0.1.0-rc.2-macos-universal.dmg"
+  url "https://github.com/TrentBrown/gatereeve/releases/download/v0.1.0-rc.6/GateReeve-0.1.0-rc.6-macos-universal.dmg"
   name "GateReeve"
   desc "Visual companion for governed agentic development workflows"
   homepage "https://gatereeve.pages.dev/"
